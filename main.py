73f1f3e33d003088ae5ecd65ff38d9e23ecebd3d
Jayson Helseth <jayson.helseth@gmail.com>
2020-05-29 19:52:32 -0500

    Connect the button and the IR sensor.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import re
+import os
 from PySide2 import QtCore
+from PySide2.QtCore import QTimer
 from PySide2.QtWidgets import QApplication, QMainWindow
 from gui import Ui_MainWindow
 
@@ -12,13 +14,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.quitButton.clicked.connect(app.exit)
         self.resetButton.clicked.connect(self.reset)
-        # self.setWindowState(QtCore.Qt.WindowState.WindowFullScreen)
+        self.setWindowState(QtCore.Qt.WindowState.WindowFullScreen)
         self.keyBuffer = []
         self.phrase = re.compile(r'www[.]acmetools[.]com')
 
         self.bossHealth.setMinimum(0)
         self.bossHealth.setMaximum(100)
         self.bossHealth.setValue(0)
+
+        # game loop timer
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.gameEvents())
+        self.timer.start()
 
     def keyPressEvent(self, event):
         self.keyBuffer.append(event.text())
@@ -29,6 +37,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.bossHealth.setValue(self.bossHealth.value() + 25)
             print("Hit!")
             self.keyBuffer = []
+
+    def gameEvents(self):
+        if self.bossHealth.value() >= 100:
+            return
+
+        """
+        Since this is developed on a machine without IR, we
+        will swallow the error.
+        """
+        try:
+            os.system("irsend SEND_ONCE NERFBLUE KEY_2")
+        except:
+            pass
 
     def reset(self):
         self.bossHealth.setValue(0)
