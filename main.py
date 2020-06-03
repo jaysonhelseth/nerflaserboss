64d9269cb8ae6f0134b4c961ff84f82f717a7f72
Jayson Helseth <jayson.helseth@gmail.com>
2020-06-03 16:50:28 -0500

    Moved the shoot shell calls to tmux and refactored.
    Hopefully this will remove some of the lag I experience on the Raspberry Pi.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import os
 from PySide2 import QtCore
-from PySide2.QtCore import QTimer
 from PySide2.QtWidgets import QApplication, QMainWindow
 from gui import Ui_MainWindow
 
@@ -11,19 +10,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         QMainWindow.__init__(self)
         self.setupUi(self)
 
-        self.quitButton.clicked.connect(app.exit)
+        self.quitButton.clicked.connect(self.quit)
         self.resetButton.clicked.connect(self.reset)
         self.setWindowState(QtCore.Qt.WindowState.WindowFullScreen)
 
         self.bossHealth.setMinimum(0)
         self.bossHealth.setMaximum(100)
         self.bossHealth.setValue(0)
-
-        # game loop timer
-        self.timer = QTimer()
-        self.timer.setInterval(1000)
-        self.timer.timeout.connect(self.gameEvents)
-        self.timer.start()
+        self.start_shoot()
 
     def keyPressEvent(self, event):
         if event.key() != QtCore.Qt.Key_Return and event.key() != QtCore.Qt.Key_Enter:
@@ -31,23 +25,24 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         if self.bossHealth.value() < 100:
             self.bossHealth.setValue(self.bossHealth.value() + 25)
-            print("Hit!")
 
-    def gameEvents(self):
         if self.bossHealth.value() >= 100:
-            return
-
-        """
-        Since this is developed on a machine without IR, we
-        will swallow the error.
-        """
-        try:
-            os.system("irsend SEND_ONCE NERFBLUE KEY_2")
-        except:
-            pass
+            self.stop_shoot()
 
     def reset(self):
+        self.stop_shoot()
         self.bossHealth.setValue(0)
+        self.start_shoot()
+
+    def start_shoot(self):
+        os.system('tmux new -d -s shoot "./shoot.sh"')
+
+    def stop_shoot(self):
+        os.system('tmux kill-session -t shoot')
+
+    def quit(self):
+        self.stop_shoot()
+        app.exit()
 
 
 if __name__ == '__main__':
---
+++ shoot.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+while true
+do
+	aplay laser.wav
+	irsend SEND_ONCE NERFBLUE KEY_2
+	sleep 1
+done
